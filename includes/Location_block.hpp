c3029e125395dcf79ac62f310f6b7490b7d7d85a
minsikim-42 <74590585+minsikim-42@users.noreply.github.com>
2022-05-13 16:09:40 +0900

    Chore :  Feat : creat config block cpp! (#21)
    * Test: plant kqueue - 5#, 8#
    
    * Feat: detach print, debug function
    
    * Feat: Socket Capsulization
    
    * Remove: remove rebunant files
    
    * Test: key parsing...
    
    * Test: block structing, pull request
    
    Co-authored-by: chan.yeop <LimChanyeop@users.noreply.github.com>
    
    * Test: http block structing
    
    * Remove: remove parseConfig -> parseUtils
    
    * Fix: parsing put objectlization
    
    * Chore : git pull origin main
    
    * Feat: creat config block cpp
    
    * Feat : creat config block cpp!
    
    Co-authored-by: chanyeop <153893@naver.com>
    Co-authored-by: chan.yeop <LimChanyeop@users.noreply.github.com>

--- includes/Location_block.hpp
+++ includes/Location_block.hpp
@@ -2,53 +2,59 @@
 #define CONFIG_LOCATION_HPP
 
 #include <iostream>
+#include "parseUtils.hpp"
 
 class Location_block
 {
 public:
-    int client_limit_body_size;
-    int request_limit_header_size;
-    std::string user;
-    std::string worker_processes;
-    std::string listen;
-    std::string server_name;
-    std::string root;
-    std::string index;
-    std::string autoindex;
-    std::string return_n;
-    std::string error_page;
-    std::string cgi_info;
-    std::string allow_methods;
-    std::string auth_key;
+	int client_limit_body_size;
+	int request_limit_header_size;
+	std::string user;
+	std::string worker_processes;
+	// std::string listen;		 // server
+	// std::string server_name; // server
+	std::string root;
+	std::string index;
+	std::string autoindex;
+	std::string return_n;
+	std::string error_page;
+	std::string cgi_info;
+	std::string allow_methods;
+	std::string auth_key;
 
 public:
-    const std::string &get_user(void) const { return user; }
-    const std::string &get_worker_processes(void) const { return worker_processes; }
-    const std::string &get_listen(void) const { return listen; }
-    const std::string &get_server_name(void) const { return server_name; }
-    const std::string &get_root(void) const { return root; }
-    const std::string &get_index(void) const { return index; }
-    const std::string &get_autoindex(void) const { return autoindex; }
-    const std::string &get_return_n(void) const { return return_n; }
-    const std::string &get_error_page(void) const { return error_page; }
-    const std::string &get_cgi_info(void) const { return cgi_info; }
-    const std::string &get_allow_methods(void) const { return allow_methods; }
-    const std::string &get_auth_key(void) const { return auth_key; }
-    const int &get_client_limit_body_size(void) const { return client_limit_body_size; }
-    const int &get_request_limit_header_size(void) const { return request_limit_header_size; }
+	const std::string &get_user(void) const;
+	const std::string &get_worker_processes(void) const;
+	// const std::string &get_listen(void) const;
+	// const std::string &get_server_name(void) const;
+	const std::string &get_root(void) const;
+	const std::string &get_index(void) const;
+	const std::string &get_autoindex(void) const;
+	const std::string &get_return_n(void) const;
+	const std::string &get_error_page(void) const;
+	const std::string &get_cgi_info(void) const;
+	const std::string &get_allow_methods(void) const;
+	const std::string &get_auth_key(void) const;
+	const int &get_client_limit_body_size(void) const;
+	const int &get_request_limit_header_size(void) const;
 
-    void set_user(std::string &str) { user = str; }
-    void set_worker_processes(std::string &str) { worker_processes = str; }
-    void set_listen(std::string &str) { listen = str; }
-    void set_server_name(std::string &str) { server_name = str; }
-    void set_root(std::string &str) { root = str; }
-    void set_index(std::string &str) { index = str; }
-    void set_autoindex(std::string &str) { autoindex = str; }
-    void set_return_n(std::string &str) { return_n = str; }
-    void set_error_page(std::string &str) { error_page = str; }
-    void set_cgi_info(std::string &str) { cgi_info = str; }
-    void set_allow_methods(std::string &str) { allow_methods = str; }
-    void set_auth_key(std::string &str) { auth_key = str; }
+	void set_user(std::string &str);
+	void set_worker_processes(std::string &str);
+	// void set_listen(std::string &str);
+	// void set_server_name(std::string &str);
+	void set_root(std::string &str);
+	void set_index(std::string &str);
+	void set_autoindex(std::string &str);
+	void set_return_n(std::string &str);
+	void set_error_page(std::string &str);
+	void set_cgi_info(std::string &str);
+	void set_allow_methods(std::string &str);
+	void set_auth_key(std::string &str);
+	void set_client_limit_body_size(int i);
+	void set_request_limit_header_size(int i);
+
+	void print_all(void) const;
+	void config_parsing(std::vector<std::string> lists); //, Config_base config_base)
 };
 
 #endif
