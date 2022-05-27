5f803d5fd0e573aeced5b2b0a50215f441dcfca0
minsikim-42 <spalstlr321@naver.com>
2022-05-27 20:34:07 +0900

    Fix: response(favicon) content-Type: image/vnd.microsoft.icon

--- includes/Webserv.hpp
+++ includes/Webserv.hpp
@@ -56,9 +56,17 @@ public:
 		}
 		else if (i == 42)
 		{
-			response_str = "HTTP/1.1 200 OK\r\nContent-Type: "
-						   "image/x-icon\r\nContent-Length: ";
-			response_str += std::to_string(str_buf.length() + 1);
+			response_str = "HTTP/1.1 200 OK\r\n";
+			response_str += "cache-control: max-age=31536000\r\n";
+			response_str += "content-encoding: gzip\r\n";
+			response_str += "content-length: ";
+			response_str += std::to_string(str_buf.size());
+			response_str += "\r\ncontent-type: image/vnd.microsoft.icon\r\n";
+			response_str += "referrer-policy: unsafe-url";
+			response_str += "server: NWS\r\n";
+			response_str += "strict-transport-security: max-age=63072000; includeSubdomains\r\n";
+			response_str += "vary: Accept-Encoding,User-Agent\r\n";
+			
 			response_str += "\r\n\r\n";
 			response_str += str_buf + "\r\n";
 		}
