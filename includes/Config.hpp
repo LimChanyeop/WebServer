d5ae07b023235175df98ce12acdb5825e560707d
Chanyeop Lim <clim@c1r8s3.42seoul.kr>
2022-05-27 22:06:04 +0900

    Refactor: initiate new version, make files

---
+++ includes/Config.hpp
@@ -0,0 +1,23 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+class Config
+{
+private:
+	std::vector<Server> v_server;
+
+public:
+	Config(/* args */);
+	~Config();
+};
+
+Config::Config(/* args */)
+{
+}
+
+Config::~Config()
+{
+}
+
+
+#endif
